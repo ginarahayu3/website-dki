f4e659ccfdf1123efb235da55cdbb13fe029cb78
Gina Rahayu <ginarahayu3@gmail.com>
2020-09-16 13:15:09 +0700

    Fungsi Menambahkan Router

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ } from "react-router-dom";
+ import Home from './Home';
+ import Register from './Register';
+
+
+function App() {
+  return (
+<Router basename='/website-dki'>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/register'>
+          <Register/>
+        </Route>
+      </Switch>
+    </Router>
+
+);
+}
+
+export default App;
